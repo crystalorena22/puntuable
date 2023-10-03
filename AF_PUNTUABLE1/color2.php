2081945efa2cb74dc648a29509642da2417892db
Crystal Sanchez <124258183+crystalorena22@users.noreply.github.com>
2023-10-03 17:43:06 +0200

    final

--- AF_PUNTUABLE1/color2.php
+++ AF_PUNTUABLE1/color2.php
@@ -5,48 +5,78 @@
         <meta charset="utf-8">
         <meta name="description" content="Numeros primos en PHP">
         <meta name="keywords" content="PHP,Formularios,Javascript,diseño web,primos">
+        <link rel="stylesheet" href="G:\Mi unidad\Servidor\puntuable\AF_PUNTUABLE1\estilo.css">
         <title>AF puntuable 1</title>
+        <style>
+           body {
+                font-family: Arial, sans-serif;
+                background-color: #c8c4c9;
+                width:450px
+                padding: 32;
+            }
+
+            h1 {
+                text-align: center;
+                font-size: 24px;
+                color: #000000;
+                margin-top: 20px;
+            }
+
+            p {
+                font-size: 18px;
+                color: #251818;
+                margin-bottom: 10px;
+            }
+
+            .container {
+                text-align: left;
+                box-sizing: border-box;
+                width: 300px;
+                padding: 16px;
+                margin: auto;
+                border-radius: 10px;
+                background-color: #c8c4c9;
+                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            }
+
+        </style>
     </head>
-
     <body>
-        <?php
-            echo "Nombre: " . $_POST['nombre']."<br>";
-            echo "Contraseña: " . $_POST['pass']."<br>";
-            echo "Coche: " . $_POST['coche']."<br>";
-            echo "Comunidad autonoma: ".$_POST['comunidad']."<br>";
-            echo "Animal: " . $_POST['animal']."<br>";
-            echo "Color: " . $_POST['color']."<br>";
-            echo "Comentario: " . $_POST['mensaje']."<br>";
-            echo "Campo oculto: " . $_POST['oculto']."<br>";
+        <div class="container">
+            <h1>Datos</h1>
+            <?php
 
+                $nombre = $_POST['nombre'];
+                echo "<p>Nombre: $nombre</p>";
 
+                $pass = $_POST['pass'];
+                echo "<p>Contraseña: $pass</p>";
 
+                $coche = $_POST['coche'];
+                echo "<p>Marca de coche: $coche</p>";
 
+                $com = $_POST['comunidad'];
+                if (!empty($com)) {
+                    echo "<p>Comunidade/s Autónoma/s: " . implode(", ", $com) . "</p>";
+                } else {
+                    echo "<p>No se seleccionaron comunidades autónomas.</p>";
+                }
 
+                $animal = $_POST['animal'];
+                echo "<p>Animal: $animal</p>";
 
-            /*
-            echo "Nombre: " . $nombre . "<br>";
-            $pass =$_POST['pass'];
-            $coche =$_POST['coche'];
-            $comun =$_POST['comunidad'];
-            $animal =$_POST['animal'];
-            $color =$_POST['color'];
-            $mensaje =$_POST['mensaje'];
-            $oculto =$_POST['oculto'];
+                $color = $_POST['color'];
+                echo "<p>Color: $color</p>";
 
-            echo "<h2>Datos del formulario:</h2>";
+                $mensaje = $_POST['mensaje'];
+                echo "<p>Comentario: $mensaje</p>";
 
-            echo "Clave: " . $pass . "<br>";
-            echo "Marca de coche: " . $coche . "<br>";
-            echo "Comunidades Autónomas seleccionadas: " . implode(", ", $comun) . "<br>";
-            echo "Animal seleccionado: " . $animal . "<br>";
-            echo "Color de fondo seleccionado: " . $color . "<br>";
-            echo "Comentario: " . $mensaje . "<br>";
-            echo "Campo oculto: " . $oculto . "<br>";
+                $hidden = $_POST['oculto'];
+                echo "<p>Oculto: $hidden</p>";
 
-            echo "<style>body{background-color: " . $color . ";}</style>";
-            */
+                echo "<style>body{background-color: $color;}</style>";
 
-        ?>
+            ?>
+        </div>
     </body>
 </html>
